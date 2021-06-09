0bd818b10c5fa17df9ce4ebebeb608f884292611
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-06-08 22:33:31 -0700

    feat: Connect the Heroku Postgres database
    Following this from Heroku docs:
    https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js

---
+++ db/index.js
@@ -0,0 +1,25 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+function getMessages(request, response) {
+  client.query("SELECT * FROM messages;", (error, results) => {
+    if (err) {
+      throw err;
+    }
+
+    response.status(200).json(results.rows);
+    client.end();
+  });
+}
+
+module.exports = {
+  getMessages,
+};
